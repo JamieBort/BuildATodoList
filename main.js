2107ae88ff5e6f91fefcae359c92ddbf30a5bf52
Jamie Bort <jamiebort@gmail.com>
2018-03-22 13:59:32 -0400

    Refactoring code to learn it better.

---
+++ main.js
@@ -0,0 +1,43 @@
+// 1. require packages DONE
+// 2. use the boiler plate engine for mustacheExpress DONE
+// 3. use exrpress.static DONE
+// 4. define variables
+// 5. write get
+// 6. write post
+// 7. write post
+// 8. use app.listen(3000....) DONE
+
+// also create home.mustache file
+
+// require various packages
+// express, mustache-express, body-parser, express-validator
+
+// boilerplate engine
+
+// use express static
+
+// use body parser
+
+// use validator
+        // this line must be immediately after any of the bodyParser middlewares
+
+
+// define variables ==========================
+
+// responses and requests
+// get information
+
+// tie these items to the ones in the mustache file
+
+
+// post todo information
+
+// post done information
+
+
+  // see http://expressjs.com/en/api.html for more info on redirect
+
+
+
+
+// open up the port
