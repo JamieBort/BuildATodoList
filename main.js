eb100689a7111920f0f5febc227913c625ebe15a
Jamie Bort <jamiebort@gmail.com>
2018-06-13 13:40:32 -0400

    Working on home.mustache file.

--- main.js
+++ main.js
@@ -13,6 +13,16 @@ const express = require('express'); // npm install express --save
 const app = express();
 const mustacheExpress = require('mustache-express'); // npm install mustache-express --save
 const bodyParser = require('body-parser') // npm install body-parser --save
+// About body-parser from: https://stackoverflow.com/questions/38306569/what-does-body-parser-do-with-express
+// To handle HTTP POST request in Express.js version 4 and above, you need to install middleware module called body-parser.
+// body-parser extract the entire body portion of an incoming request stream and exposes it on req.body.
+// The middleware was a part of Express.js earlier but now you have to install it separately.
+// This body-parser module parses the JSON, buffer, string and URL encoded data submitted using HTTP POST request. Install body-parser using NPM as shown below.
+// npm install body-parser --save
+
+
+
+
 const expressValidator = require('express-validator'); // npm install express-validator --save
 
 // boilerplate engine
@@ -44,15 +54,21 @@ let myintegervariable = 0;
 // res.render('home', { // populate home.mustache view
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('home', { title: 'Hey', message: 'Hello there!' })
 });
 
 // tie these items to the ones in the mustache file
 
 
 // post todo information
+app.post('/', function (req, res) {
+  res.send('POST request to homepage');
+});
 
 // post done information
+app.post('/', function (req, res) {
+  res.post('Post again');
+})
 
 
 // see http://expressjs.com/en/api.html for more info on redirect
